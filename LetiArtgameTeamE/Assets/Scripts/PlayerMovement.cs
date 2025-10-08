b0a046f69148b786940e60f461926f079f4f83b7
whitewolf18 <169000698+whitewolf18@users.noreply.github.com>
2025-10-08 14:02:36 +0200

    The jump has been added
    space bar is to jump, A/LEFT ARROW is to go left and d/RIGHT ARROW is right. play can still fall off the map

--- LetiArtgameTeamE/Assets/Scripts/PlayerMovement.cs
+++ LetiArtgameTeamE/Assets/Scripts/PlayerMovement.cs
@@ -2,13 +2,16 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class PlayerMovement : MonoBehaviour {
-
+public class PlayerMovement : MonoBehaviour
+{
     public float speed = 5;
+    public float jumpForce = 5f; // how high the player jumps
     public Rigidbody rb;
 
     float horizontalInput;
     public float horizontalMultiplier = 2;
+
+    bool isGrounded = true; // check if player is on the ground
 
     private void FixedUpdate()
     {
@@ -17,10 +20,29 @@ public class PlayerMovement : MonoBehaviour {
         rb.MovePosition(rb.position + forwardMove + horizontalMove);
     }
 
-
     private void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
+
+        // jump input
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
     }
 
+    private void Jump()
+    {
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        isGrounded = false;
+    }
+
+    // detect when player lands back on the ground
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
 }
