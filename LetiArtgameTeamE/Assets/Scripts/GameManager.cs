657483666c02fa3a2bb12799b7931725fb585f50
whitewolf18 <169000698+whitewolf18@users.noreply.github.com>
2025-10-10 11:32:44 +0200

    coins and high score adds

---
+++ LetiArtgameTeamE/Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class GameManager : MonoBehaviour
+{
+
+    public int score;
+    public static GameManager inst;
+
+    public Text scoreText;
+
+    public void IncrementScore()
+    {
+        score++;
+        scoreText.text = "SCORE: " + score;
+    }
+
+    private void Awake()
+    {
+        inst = this;
+    }
+
+    private void Start()
+    {
+
+    }
+
+
+    private void Update()
+    {
+
+    }
+
+}
--- LetiArtgameTeamE/Assets/Scripts/GroundTile.cs
+++ LetiArtgameTeamE/Assets/Scripts/GroundTile.cs
@@ -6,12 +6,41 @@ public class GroundTile : MonoBehaviour
 
     private void Start()
     {
-        groundSpawner = FindObjectOfType<GroundSpawner>();
+        groundSpawner = GameObject.FindObjectOfType<GroundSpawner>();
+        SpawnCoins();
     }
 
     private void OnTriggerExit(Collider other)
     {
         groundSpawner.SpawnTile();
         Destroy(gameObject, 2);
+    }
+
+    public GameObject coinPrefab;
+
+    void SpawnCoins ()
+    {
+        int coinsToSpawn = 10;
+        for (int i = 0; i < coinsToSpawn; i++)
+        {
+           GameObject temp = Instantiate(coinPrefab, transform);
+            temp.transform.position = GetRandomPointInCollider(GetComponent<Collider>());
+        }
+    }
+
+    Vector3 GetRandomPointInCollider (Collider collider)
+    {
+        Vector3 point = new Vector3(
+            Random.Range(collider.bounds.min.x, collider.bounds.max.x),
+            Random.Range(collider.bounds.min.y, collider.bounds.max.y),
+            Random.Range(collider.bounds.min.z, collider.bounds.max.z)
+            );
+        if (point != collider.ClosestPoint(point))
+        {
+            point = GetRandomPointInCollider(collider);
+        }
+
+        point.y = 1;
+        return point;
     }
 }
