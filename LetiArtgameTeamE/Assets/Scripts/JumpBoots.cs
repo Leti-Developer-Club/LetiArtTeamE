c30be37b71ce7462a3ba7947438d817291de255b
whitewolf18 <169000698+whitewolf18@users.noreply.github.com>
2025-10-21 21:03:41 +0200

    Yaseen coin and high score (#5)
    * coins and high score adds
    
    * hey this is new one
    
    Loop and Obstacles
    loop works when you hit and object it restarts
    
    * this is updated
    
    * new update end screen with some text and I added audeo for game loop as well as a menu scene
    
    * power up add
    
    this dose work
    
    * this had magnet and slide
    
    * add new power ups
    
    * edited added audio code
    
    * new update
    
    * updated with benitos code
    
    * VS,001
    
    * HI
    
    * Update
    
    I think all the bugs are gone

--- LetiArtgameTeamE/Assets/Scripts/JumpBoots.cs
+++ LetiArtgameTeamE/Assets/Scripts/JumpBoots.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class JumpBoots : MonoBehaviour
 {
-    public float jumpBoost = 2f;            // Jump multiplier
-    public float boostDuration = 5f;        // Duration of boost
-    public GameObject pickupEffect;
+    [Header("Jump Boost Settings")]
+    public float jumpBoost = 2f;        // Extra jump force amount
+    public float boostDuration = 5f;    // Duration of boost
+    public GameObject pickupEffect;     // Optional pickup VFX
+
+    private const float baseJumpForce = 5f; // Normal jump force
 
     private void OnTriggerEnter(Collider other)
     {
@@ -17,25 +20,35 @@ public class JumpBoots : MonoBehaviour
 
     void Pickup(Collider player)
     {
+        // Play pickup VFX
         if (pickupEffect != null)
             Instantiate(pickupEffect, transform.position, transform.rotation);
 
+        // Apply boost
         PlayerMovement movement = player.GetComponent<PlayerMovement>();
         if (movement != null)
         {
-            StartCoroutine(ApplyJumpBoost(movement));
+            movement.StartCoroutine(ApplyJumpBoost(movement));
         }
 
+        // Destroy the power-up object
         Destroy(gameObject);
     }
 
     IEnumerator ApplyJumpBoost(PlayerMovement movement)
     {
-        float originalJumpForce = movement.jumpForce;
-        movement.jumpForce += jumpBoost;
+        // Boost jump
+        movement.jumpForce = baseJumpForce + jumpBoost;
+        Debug.Log("Jump Boost Activated! New jump force: " + movement.jumpForce);
 
+        // Wait for duration
         yield return new WaitForSeconds(boostDuration);
 
-        movement.jumpForce = originalJumpForce;
+        // Reset to normal
+        if (movement != null)
+        {
+            movement.jumpForce = baseJumpForce;
+            Debug.Log("Jump Boost Ended. Jump force reset to: " + movement.jumpForce);
+        }
     }
 }
