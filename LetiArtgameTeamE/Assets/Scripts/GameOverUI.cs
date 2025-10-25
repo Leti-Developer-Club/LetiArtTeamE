57d73569c76f8924fe6c95bed2b45c330bf38b29
whitewolf18 <169000698+whitewolf18@users.noreply.github.com>
2025-10-25 11:58:38 +0200

    hi still working on ui

---
+++ LetiArtgameTeamE/Assets/Scripts/GameOverUI.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class GameOverUI : MonoBehaviour
+{
+    private Button restartButton;
+    private Button mainMenuButton;
+
+    void Start()
+    {
+        var uiDocument = GetComponent<UIDocument>();
+        var root = uiDocument.rootVisualElement;
+
+        restartButton = root.Q<Button>("RestartButton");
+        mainMenuButton = root.Q<Button>("MainMenuButton");
+
+        if (restartButton != null)
+            restartButton.clicked += RestartLevel;
+
+        if (mainMenuButton != null)
+            mainMenuButton.clicked += ReturnToMainMenu;
+    }
+
+    private void RestartLevel()
+    {
+        Debug.Log("🔁 Restarting current level...");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private void ReturnToMainMenu()
+    {
+        Debug.Log("🏠 Returning to Main Menu...");
+        SceneManager.LoadScene("MainMenu");
+    }
+}
+
